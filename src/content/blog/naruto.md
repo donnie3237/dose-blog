a206e7ef84f1a491945ead0a8b8f548a081a6966
donnie3237 <paradorn3237@gmail.com>
2023-11-11 09:08:08 +0700

    .

---
+++ src/content/blog/naruto.md
@@ -0,0 +1,13 @@
+---
+title: naruto007
+author: Sat Naing
+pubDatetime: 2023-11-10T03:42:51Z
+postSlug: how-do-i-develop-my-terminal-portfolio-website-with-react
+featured: false
+draft: false
+tags:
+  - naruto
+  - eiei
+description:
+  "EXAMPLE POST: ทาวริ เป็นเฟรมเวิร์คสำหรับสร้าง เนทีฟแอพ สำหรับมือถือ และ PC นอกจากนี้มันยังมีประสิทธิภาพที่ไม่ธรรมดาด้วยภาษา Rust"
+---
---
+++ src/utils/project.ts
@@ -0,0 +1,44 @@
+export const myProject : any[] = [
+    { 
+        id: 1, 
+        url: "https://calender-web.vercel.app/", 
+        text: "Calender",  
+        class:"calender img",
+        subtitle: "ระบบจองเวลา"
+    },
+    { 
+        id: 15, 
+        url: "/", 
+        text: "Crud",  
+        class:"crud img",
+        subtitle: "ระบบcrud พื้นฐาน"
+    },
+    { 
+        id: 2, 
+        url: "/", 
+        text: "Statistics" , 
+        class:"statistic img",
+        subtitle: "การแสดงผลข้อมูล แดชบอร์ด เบื้องต้น"
+    },
+    { 
+        id: 3, 
+        url: "https://dose-ai.netlify.app/", 
+        text: "Image classification" ,
+        class:"neural img",
+        subtitle: "AI ตรวจจับวัตถุ"
+    },
+    { 
+        id: 4, 
+        url: "https://solid-ecom.vercel.app/", 
+        text: "Ecommerce" ,
+        class:"ecommerce img" ,
+        subtitle: "ความปลอดภัยของข้อมูล และประสิทธิภาพ"
+    },
+    { 
+        id: 6, 
+        url: "https://pdf-indol.vercel.app/", 
+        text: "PDF-Generater" , 
+        class:"pdf img" ,
+        subtitle: "การดึงAPIและระบบไฟล์"
+    }
+]
