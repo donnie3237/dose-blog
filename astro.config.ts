11ba6165a22eace7401aa6a214722f41306cc3d2
D0NN1E <paradorn3237@gmail.com>
2023-11-28 11:53:13 +0700

    .

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
   output:'server',
   adapter: vercel({
     edgeMiddleware: true,
+    speedInsights: {
+      enabled: true,
+    },
   }),
   integrations: [
     tailwind({
